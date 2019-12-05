70934ba8ce777cd0b87071d42de2bca1d1b176c1
padumamadushanka <madushankapaduma@gmail.com>
2019-12-05 11:04:52 +0530

    added filrs to homepage and used create-react-app to enerate project

---
+++ src/homepage.component.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./homepage.styles.scss";
+const HomePage = () => (
+  <div className="homePage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SMART PHONES</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SMART WATCHES</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">LAPTOPS</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMENS</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+export default HomePage;
