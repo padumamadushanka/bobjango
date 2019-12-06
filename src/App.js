83f6c0d2029ed69785fc8a0ff00ff35900c05df7
padumamadushanka <madushankapaduma@gmail.com>
2019-12-06 13:25:23 +0530

    adding header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
-
+import Header from "./components/header/header.component";
+// //we put all our header and continuosly needed components outside switch
+// it will always render
 function App() {
   return (
     <div>
+      <Header></Header>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/shop" component={ShopPage}></Route>
